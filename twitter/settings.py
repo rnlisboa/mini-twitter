4e057d01ac12d807c1a87001817753357b5197fe
Renan Lisboa <dev.renanlisboa@gmail.com>
2023-05-28 18:22:15 -0300

    atualiza settings

--- twitter/settings.py
+++ twitter/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = django-insecure-o0s+z)rzy*25khgt)si=%)dz5s-t-juy4bt**dodpy3hkb9*^1
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -95,7 +95,7 @@ SIMPLE_JWT = {
     'UPDATE_LAST_LOGIN': False,
 
     'ALGORITHM': 'HS256',
-    'SIGNING_KEY':  config('SECRET_KEY'),
+    'SIGNING_KEY':  django-insecure-o0s+z)rzy*25khgt)si=%)dz5s-t-juy4bt**dodpy3hkb9*^1,
     'VERIFYING_KEY': None,
     'AUDIENCE': None,
     'ISSUER': None,
